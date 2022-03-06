a6aaf8f9455925da2747e611781761a05090658c
Kenyon Smith <kenyon.smith@aggiemail.usu.edu>
2022-03-05 20:49:05 -0700

    Pull CVTD data and print to screen

--- Makefile
+++ Makefile
@@ -1,3 +1,32 @@
-all: 
-	gcc -o cvtdserver cvtdserver.c
+OBJDIR := obj
+BINDIR := bin
+CC     := gcc
+CFLAGS := 
+
+# Object file groups
+MOBJ := $(OBJDIR)/main.o
+TPOBJ := $(OBJDIR)/mongoose/mongoose.o
+
+# Depend groups
+TPDEP := mongoose/mongoose.h
+
+# Primary target
+all: $(MOBJ) $(TPOBJ)
+	@mkdir -p $(BINDIR)
+	$(CC) -o $(BINDIR)/cvtdserver $(MOBJ) $(TPOBJ)
+
+# Target to build third party libraries
+$(TPOBJ): mongoose/mongoose.c $(TPDEP)
+	@mkdir -p $(OBJDIR)/mongoose
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+# Target to build main file (which doesn't have an include file)
+$(MOBJ): main.c $(TPDEP)
+	@mkdir -p $(OBJDIR)
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+# Phony target
+clean:
+	rm -rf obj
+	rm -rf bin
 
--- main.c
+++ main.c
@@ -1,6 +1,61 @@
+#include "mongoose/mongoose.h"
+
+static const char* pCvtdUri = "http://cvtd.info:8080/CVTDfeed/V200/XML/_System.php?key=%s";
+char pCvtdKey[32];
+char pCvtdUriWithKey[256];
+
+static void fn(struct mg_connection* c, int ev, void* ev_data, void* fn_data)
+{
+    if (ev == MG_EV_CONNECT)
+    {
+        // Connected to server
+        printf("Connected to server\n");
+        struct mg_str host = mg_url_host(pCvtdUriWithKey);
+
+        // Send request
+        mg_printf(c, "GET %s HTTP/1.0\r\n"
+                     "Host: %.*s\r\n"
+                     "\r\n",
+                     mg_url_uri(pCvtdUriWithKey), (int) host.len, host.ptr);
+    } else if (ev == MG_EV_HTTP_MSG)
+    {
+        // Response is received. Print it
+        struct mg_http_message* hm = (struct mg_http_message*) ev_data;
+        printf("%.*s", (int) hm->message.len, hm->message.ptr);
+        c->is_closing = 1;          // Tell mongoose to close this connection
+        *(bool*) fn_data = true;    // Tell event loop to stop
+    } else if (ev == MG_EV_ERROR)
+    {
+        printf("There was an error\n");
+        *(bool*) fn_data = true;
+    }
+}
+
 int main(int argc, char* argv[])
 {
+    struct mg_mgr mgr;
+    bool done = false;
 
-    :
+    // Open up key.txt and fill out pCvtdUriWithKey
+    FILE* pKeyFile = fopen("key.txt","r");
+    if (pKeyFile == NULL)
+    {
+        printf("Error: Could not find key.txt\n");
+        return -1;
+    }
+    fread(pCvtdKey, sizeof(char), 32, pKeyFile);
+    sprintf(pCvtdUriWithKey, pCvtdUri, pCvtdKey);
+    fclose(pKeyFile);
+
+    mg_mgr_init(&mgr);
+    mg_http_connect(&mgr, pCvtdUriWithKey, fn, &done);
+
+    while (!done)
+    {
+        mg_mgr_poll(&mgr, 1000);
+    }
+
+    mg_mgr_free(&mgr);
+
     return 0;
 }
